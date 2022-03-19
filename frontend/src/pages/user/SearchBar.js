a4ef1c08aaf5c98458d6680c1b3a3cc178b4701a
Simeon <simeon.mirceski@gmail.com>
2022-03-19 15:49:39 +0100

    reset user search on 0 characters
    reset user search on 0 characters

--- frontend/src/pages/user/SearchBar.js
+++ frontend/src/pages/user/SearchBar.js
@@ -1,25 +1,25 @@
 import { TextField } from "@material-ui/core";
 import { SearchUsers } from "../../services/UserService";
 import { UserRole } from "../../services/UserService";
+import { Grid } from "@material-ui/core";
 
 const SearchBar = (props) => {
 
     const handleChange = (e) => {
     let input = e.target.value
-        if(input.length >= 3) {
+        if(input.length >= 3 || input.length == 0) {
             SearchUsers(input, UserRole.User).then((res) => props.setUsers(res.data))
         }
     }
 
    return ( 
-   <div>
-        <div>
+        <Grid container justify="centered">
             <TextField
             onChange = { e => handleChange(e) } 
             label="Search"
+            justify="center"
             />
-        </div>
-    </div>
+        </Grid>
    )
 };
 

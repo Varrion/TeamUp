6d08f1bb9f0d88f47093e6856993a8cb08c45dc9
Dejan Mancheski <dejan@3pdevelopment.com>
2022-04-10 14:32:55 +0200

    sportsman page design changes

--- frontend/src/pages/SportsmanPage.js
+++ frontend/src/pages/SportsmanPage.js
@@ -1,45 +1,55 @@
-import {Grid, Typography} from "@material-ui/core";
+import { Grid, Typography } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
-import {navigate} from "@reach/router";
+import { navigate } from "@reach/router";
 
 const SportsmanPage = () => {
     return (
         <div>
-            <Typography variant={"h3"} className={"text-center mt-3"}>Sportsman</Typography>
+            <Typography variant={"h3"} className={"text-center mt-5 "}>Sportsman</Typography>
             <Grid container className={"mt-5"}>
-                <Grid item md={7} xl={6} className={"p-4"}>
-                    <Typography variant={"h6"}>
-                        Sport’s main purposes are to promote physical activity and improve
-                        motor skills for health and performance and psychosocial development. <br/>
-                        Positive effects from sports are achieved primarily through physical activity, but
-                        secondary effects bring health benefits such as psychosocial and personal development
-                        and less alcohol consumption.
-                    </Typography>
-                    <Typography variant={"h6"}>
-                        Human biology requires a certain amount of physical activity to maintain good health and wellbeing.<br/>
-                        Participants also gain a chance to be part of a community, develop new social circles, and create social norms and attitudes.
-                        Taking part in sport can help us feel fitter, healthier and mentally strong, and that is just the start of it. Sport can also be fun,
-                        especially when played as part of a team or with family or friends.<br/><br/>
+                <Grid item xs={6} style={{ padding: "0px 40px 20px 60px" }}>
+                    <Typography variant={"h6"} style={{ fontWeight: "600", maxWidth: "700px" }}>
+                        <p>
+                            Sport’s main purposes are to promote physical activity and improve
+                            motor skills for health and performance and psychosocial development.
+                        </p>
+                        <p>
+                            Positive effects from sports are achieved primarily through physical activity, but
+                            secondary effects bring health benefits such as psychosocial and personal development
+                            and less alcohol consumption.
+                        </p>
+                        <p>  Human biology requires a certain amount of physical activity to maintain good health and wellbeing.</p>
+                        <p>  Participants also gain a chance to be part of a community, develop new social circles, and create social norms and attitudes.</p>
+                        <p> Taking part in sport can help us feel fitter, healthier and mentally strong, and that is just the start of it. Sport can also be fun,
+                            especially when played as part of a team or with family or friends.<br /><br /></p>
                     </Typography>
 
-                    <Typography variant={"h4"}>Benefits od playing sports:</Typography>
-                    - Better Sleep <br/>
-                    - A Strong Heart<br/>
-                    - New Connections<br/>
-                    - Improved Lung Function<br/>
-                    - Reduces Stress<br/>
-                    - Improve Mental Health<br/>
 
-                    <br/><br/><br/>
+                </Grid>
+
+                <Grid item xs={6} className="pr-4"  >
+                    <img src={require('../assets/images/sportsman.jpg')} className={"full-width"} alt={"photo"} style={{borderRadius:"10px"}} />
+                </Grid>
+                <Grid item xs={12}>
                     <div className={"text-center mt-5"}>
                         <Button variant="contained" size={"large"}
-                                onClick={() => navigate("/register", {state: {roleType: false}})}
-                                className={"role-card-button"}> JOIN US NOW
+                            onClick={() => navigate("/register", { state: { roleType: false } })}
+                            className={"role-card-button"}> JOIN US NOW
                         </Button>
                     </div>
                 </Grid>
-                <Grid item md={5} xl={6}>
-                    <img src={require('../assets/images/sportsman.jpg')} className={"full-width"} alt={"photo"}/>
+                <Grid item xs={12} className={"mt-5 text-center"}>
+                    <Typography variant={"h4"}>Benefits od playing sports:</Typography>
+                    <Typography variant={"h6"} className="mt-4">
+                        - Better Sleep <br />
+                        - A Strong Heart<br />
+                        - New Connections<br />
+                        - Improved Lung Function<br />
+                        - Reduces Stress<br />
+                        - Improve Mental Health<br />
+                    </Typography>
+
+                    <br /><br /><br />
                 </Grid>
             </Grid>
 
